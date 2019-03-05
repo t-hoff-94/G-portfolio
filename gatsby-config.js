70952c1a2631f18ec984f751d628944ca1497f14
Tanner <tannerjhoffman@gmail.com>
2019-03-05 08:51:59 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Tanner Hoffman`,
+    author: `Tanner Hoffman`,
+    description: `My portfolio using Gatsby Starter Blog.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `tannerhoffman`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -38,12 +38,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              Hey, i'm <strong>{author}</strong>, a Web Designer/ Developer who lives in Salt Lake City and enjoys building cool things.
             </p>
           </div>
         )
@@ -54,7 +49,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile.jpg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
